ad818c03017b1864c38fa408c6b07c4edd0712b9
Andrew Bohm <andrew.bohm@pacificuniform.com>
2021-04-21 12:08:27 -0700

    Added LS_COLORS install

--- TheBomb/install.sh
+++ TheBomb/install.sh
@@ -13,3 +13,12 @@ for f in `find * -regex ".*\.vim$\|.*\.lua$"`; do
     rm -rf ~/.config/nvim-nightly/$f
     ln -s ~/dotfiles/TheBomb/$f ~/.config/nvim-nightly/$f
 done
+
+read -p "Do you want to install LS_COLORS for zsh: (y/n) " -n 1 -r
+echo "More info: https://github.com/trapd00r/LS_COLORS"
+echo "Installing LS_COLORS"
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    mkdir /tmp/LS_COLORS && curl -L https://api.github.com/repos/trapd00r/LS_COLORS/tarball/master | tar xzf - --directory=/tmp/LS_COLORS --strip=1
+    ( cd /tmp/LS_COLORS && sh install.sh )
+fi
